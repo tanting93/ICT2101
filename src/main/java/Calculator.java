515b2a629eb87f9e941d9bb19b1a94f89ba70e62
tanting93 <tanting@Tans-MacBook-Pro.local>
2016-11-10 10:29:50 +0800

    fixed error

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
   public int evaluate(String expression) {
-    int sum = 0
+    int sum = 0;
     for (String summand: expression.split("\\+"))
       sum += Integer.valueOf(summand);
     return sum;
